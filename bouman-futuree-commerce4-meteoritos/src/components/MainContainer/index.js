3b83a908d810cca3107d0691929d4759569b3fd8
Your Name <you@example.com>
2019-12-05 12:04:17 -0300

    primeiro commit

--- bouman-futuree-commerce4-meteoritos/src/components/MainContainer/index.js
+++ bouman-futuree-commerce4-meteoritos/src/components/MainContainer/index.js
@@ -12,48 +12,48 @@ const listaDeProdutos = [
     urlImagem: "http://www.sun.org/uploads/meteoriteimages/4/mainimage_Seymchan_-_357g_4.JPG",
 valor: 10000,
 nome: "Seymchan"
-  }
+  },
   {
     id:2,
     urlImagem: "http://tierra.rediris.es/megacryometeors/feliz1.jpg",
 valor: 10000,
 nome: "megacryometeors"
-  }
+  },
   {
     id:3,
     urlImagem: "https://upload.wikimedia.org/wikipedia/commons/5/5d/Santa_Rosa_meteorite%2C_main_mass.jpg",
 valor: 10000,
 nome: "Santa Rosa"
-  }
+  },
   {
     id:4,
     urlImagem: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Meteorito_Mar%C3%ADlia.jpg",
 valor: 10000,
 nome: "Meteorito Marília"
-  }
+  },
   {
     id:5,
     urlImagem: "https://ogimg.infoglobo.com.br/in/23934277-c8c-d63/FT1086A/652/xmeteorito.jpg.pagespeed.ic.Yggigm2Af9.jpg",
 valor: 10000,
 nome: "X Meteorito"
-  }
+  },
   {
     id:6,
-    urlImagem: "http://1.bp.blogspot.com/-DqZwiQdbrxM/VOt_kye5ZwI/AAAAAAAAAyI/nwuvx1lDN9k/s1600/Meteorito-Fukang1.jpg"
+    urlImagem: "http://1.bp.blogspot.com/-DqZwiQdbrxM/VOt_kye5ZwI/AAAAAAAAAyI/nwuvx1lDN9k/s1600/Meteorito-Fukang1.jpg",
 valor: 10000,
 nome: "Meteorito Fukang"
-  }
+  },
   {
     id:7,
     urlImagem: "https://i.ebayimg.com/images/g/JP8AAOSwg7pbGVSL/s-l640.jpg",
-valor: 10000,
-nome: "Mosaico"
-  }
+    valor: 10000,
+    nome: "Mosaico"
+  },
   {
     id:8,
     urlImagem: "http://www.sun.org/uploads/meteoriteimages/62/mainimage_Seymchan_Slice_61.8g_4.jpg",
-valor: 10000,
-nome: "Seymchan Slice"
+    valor: 10000,
+    nome: "Seymchan Slice"
   }
 ]
 
