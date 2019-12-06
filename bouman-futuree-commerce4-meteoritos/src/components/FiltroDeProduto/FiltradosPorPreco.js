ada548377c3e67d423f0e59f7766164fc3cf5e9e
Your Name <you@example.com>
2019-12-06 14:18:23 -0300

    Tentativa de fazer a filtragem 1

--- bouman-futuree-commerce4-meteoritos/src/components/FiltroDeProduto/FiltradosPorPreco.js
+++ bouman-futuree-commerce4-meteoritos/src/components/FiltroDeProduto/FiltradosPorPreco.js
@@ -3,12 +3,22 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 function FiltradosPorPreco(props) {
+    const produtosFiltrados = props.valor.filter((cadaValor) =>{
+        if(cadaValor >0){
+            return true
+        }else{
+            return false
+        }
+        
 
+    })
+
+    console.log(produtosFiltrados)
     return (
         <div>
             <ul>
-                {props.lista.map(valores => {
-                    return <li>{valor}</li>;
+                {props.valor.map(cadaValor => {
+                    return <li>{cadaValor}</li>;
                 })}
             </ul>
         </div>
